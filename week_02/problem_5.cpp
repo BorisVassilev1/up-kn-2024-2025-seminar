1061f2e756fac86cf3dc0ca4420417a08bb2b947
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-10-16 16:54:55 +0300

    typo

--- week_02/problem_5.cpp
+++ week_02/problem_5.cpp
@@ -4,11 +4,9 @@ int main() {
 	int year;
 	std::cin >> year;
 
-	if (year % 4 == 0) {
-		std::cout << year << std::endl;
-		return 0;
+	if (year % 4 != 0) {
+		year += 4 - (year % 4);
 	}
-	year += 4 - (year % 4);
 
 	if (year % 100 == 0 && year % 400 != 0) year += 4;
 
